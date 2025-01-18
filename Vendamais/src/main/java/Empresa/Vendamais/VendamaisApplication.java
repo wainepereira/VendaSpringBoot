1dbe9747bed66dfd41c8333bcb87f1b8a5bca695
wainepereira <wainepereira@gmail.com>
2025-01-18 15:08:18 -0300

    Inicio do projeto de vendas spring

---
+++ Vendamais/src/main/java/Empresa/Vendamais/VendamaisApplication.java
@@ -0,0 +1,13 @@
+package Empresa.Vendamais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendamaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendamaisApplication.class, args);
+	}
+
+}
---
+++ Vendamais/src/test/java/Empresa/Vendamais/VendamaisApplicationTests.java
@@ -0,0 +1,13 @@
+package Empresa.Vendamais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendamaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
